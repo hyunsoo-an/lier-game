88adff37f55cb164c9c921fc99f1bc19dd57d189
Hyunsoo An <hankjudolove@gmail.com>
2025-01-30 19:59:09 +1300

    added more seeds

--- server/db/seeds/lier.js
+++ server/db/seeds/lier.js
@@ -5,5 +5,6 @@ export async function seed(knex) {
   // Inserts seed entries
   await knex('lier').insert([
     { id: 1, word1: 'banana', word2: 'apple', topic: 'fruit' },
+    { id: 2, word1: 'kiwi', word2: 'dragon fruit', topic: 'fruit' },
   ])
 }
